557eb3bb3cc5ddd1f7ab8a6e459586a0bdb22d84
Matthew Dillon <matthewrdillon@gmail.com>
2019-08-07 13:00:17 -0700

    BLD: Scaffold the basics

---
+++ demo/source/conf.py
@@ -0,0 +1,9 @@
+project = 'Demo Documentation'
+copyright = '2019, q2d2'
+author = 'q2d2'
+extensions = [
+]
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'qiime2'
+html_static_path = ['_static']
