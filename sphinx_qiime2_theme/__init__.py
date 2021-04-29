869565b992c3e9b934db10947bf65628c075f5e1
Liz Gehret <elizabeth.gehret@gmail.com>
2021-04-29 16:47:22 -0700

    Updating license and copyright years

--- sphinx_qiime2_theme/__init__.py
+++ sphinx_qiime2_theme/__init__.py
@@ -1,5 +1,12 @@
-from os import path
+# ----------------------------------------------------------------------------
+# Copyright (c) 2019-2021, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 
+from os import path
 
 def setup(app):
     app.add_html_theme('qiime2', path.abspath(path.dirname(__file__)))
